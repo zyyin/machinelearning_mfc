995f4fb6aee8d5f24aedeedc53de28d15a37431a
zyyin <yindoor@126.com>
2017-10-03 22:39:05 +0800

    svm: sequential minimal optimization

--- ML/svm.cpp
+++ ML/svm.cpp
@@ -12,37 +12,119 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-*/
 
+data,label=loadDataSet("testSet.txt")
+smoSimple(data, label, 0.6, 0.001, 40)
+
+*/
 
 #include "stdafx.h"
 #include "MachineLearning.h"
 #include <string>
+#include <stdlib.h>
 #include "Utils.h"
 #include "xFile.h"
 
+
+int selectJrand(int i, int m)
+{
+	int j = i;
+	while(j == i)
+	{
+		j = rand()%(m);
+	}
+
+	return j;
+}
+float clipAlpha(float data, float L, float H)
+{
+	return min(H, max(L, data));
+}
 int smoSimple(Mat& src, Mat& labels, float C, float toler, int maxIter, float& b, Mat& ret)
 {
     Mat labelMat;
 	transpose(labels, labelMat);
+	srand(time(0));
+
     int m = src.rows;
 	int n = src.cols;
-	Mat alphas(m, 1, CV_32F);
-	alphas = 0;
+	Mat alphas = Mat::zeros(m, 1, CV_32F);
+
 	int iter = 0;
 	while(iter < maxIter)
 	{
 		int alphaChanged = 0;
 		for( int i = 0; i < m; i++)
 		{
-			Mat al = alphas*labelMat;
-			Mat dd = src*(src.row(i));
-			Mat ad = al.t()*dd.t();
+			Mat al = alphas.mul(labels);
+			Mat dd = src*(src.row(i).t());
+			
+			Mat ad = al.t()*dd;
 			float fxi = ad.at<float>(0, 0) + b;
-			cout<<fxi<<endl;
-		}
-	}
+	
+			float fli = labels.at<float>(i, 0);
+			float Ei = fxi - fli;
+			float iOld = alphas.at<float>(i, 0);
+			float L, H;
+			if((fli*Ei < -toler && alphas.at<float>(i, 0) < C) || 
+				(fli*Ei > toler && alphas.at<float>(i, 0) > 0))
+			{
+				int j = selectJrand(i, m);
+				
+				Mat ddj = src*(src.row(j).t());
+				Mat adj = al.t()*ddj; 
+				float fxj = adj.at<float>(0, 0) + b;
+				float flj = labels.at<float>(j, 0);
+				float Ej = fxj - flj;
+				float jOld = alphas.at<float>(j, 0);
+				if(fli != flj){
+					L = max(0, jOld-iOld);
+					H = min(C, C+jOld-iOld);
+				} else {
+					L = max(0, jOld +iOld - C);
+					H = min(C, jOld+iOld);
+				}
+				if(L== H)
+				{
+					cout<< "L==H"<< endl;
+					continue;
+				}
+				Mat diff = 2.0 * src.row(i)*src.row(j).t() - src.row(i)*src.row(i).t() - src.row(j)*src.row(j).t();
+				float eta = diff.at<float>(0 ,0);
+				if(eta > 0)
+				{
+					cout<<"eta > 0"<<endl;
+					continue;
+				}
+				float changes = flj*(Ei-Ej)/eta;
+				alphas.at<float>(j, 0) -= changes;
 
+		
+				alphas.at<float>(j, 0)= clipAlpha(alphas.at<float>(j, 0), L,H);
+
+				if (abs(alphas.at<float>(j, 0) - jOld) < 0.00001) {
+					cout<<"j not moving enough"<<endl;
+					continue;
+				}
+				alphas.at<float>(i, 0) += flj*fli*(jOld-alphas.at<float>(j, 0));
+				diff =  fli*(alphas.at<float>(i, 0)-iOld)*src.row(i)*src.row(i).t() - flj*(alphas.at<float>(i, 0) - jOld)*src.row(i)*src.row(j).t();
+				float b1 = b - Ei - diff.at<float>(0 ,0);
+				diff = fli*(alphas.at<float>(i, 0)-iOld)*src.row(i)*src.row(j).t() - flj*(alphas.at<float>(i, 0) - jOld)*src.row(j)*src.row(j).t();
+				float b2 = b - Ej - diff.at<float>(0 ,0);
+				if(alphas.at<float>(i, 0) > 0 && C > alphas.at<float>(i, 0)) b = b1;
+				else if(alphas.at<float>(j, 0) > 0 && C > alphas.at<float>(j, 0)) b = b2;
+				else b = (b1 + b2) / 2.0f;
+				alphaChanged += 1;
+
+			}
+			
+		}
+		if(!alphaChanged) iter++;
+		else iter = 0;
+		cout<<"iter: "<<iter <<endl;
+
+	}
+	ret = alphas.clone();
 	return 0;
 }
 
@@ -79,6 +161,11 @@ void testSVM()
 {
 	Mat data, labels;
 	loadDataSet("testSet.txt", data, labels);
-	cout<<data<<endl;
-	cout<<labels;
+	float b = 0; Mat ret;
+	smoSimple(data, labels, 0.6, 0.001, 40, b, ret);
+
+	cout<<"##################################################################"<<endl;
+	cout<<b<<endl;
+	cout<<ret<<endl;
+
 }
